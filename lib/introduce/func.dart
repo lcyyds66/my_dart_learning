c54665e8a9ee0d7041ed51a46cff697c6a92c539
lcyyds66 liuChang <2740491279@qq.com>
2024-04-11 13:53:56 +0000

    test0411

--- lib/introduce/func.dart
+++ lib/introduce/func.dart
@@ -2,8 +2,6 @@ int fibonacci(int n) {
   if (n == 0 || n == 1) return n;
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
-
-void main(List<String> args) {
+void main(){
   var result = fibonacci(20);
-  print(result);
 }
